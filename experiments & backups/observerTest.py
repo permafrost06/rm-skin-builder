3e666986899622455377e59d3ee97e96f682dc1b
Zahin Azmayeen <zahin06@gmail.com>
2020-09-04 06:46:06 +0600

    changed the test for more testing :p

--- experiments & backups/observerTest.py
+++ experiments & backups/observerTest.py
@@ -1,18 +1,20 @@
-import sys
+#!/usr/bin/python3
 import time
-import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    event_handler = MyHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, path='/data/', recursive=False)
     observer.start()
+
     try:
         while True:
             time.sleep(1)
